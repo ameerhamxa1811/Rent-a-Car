71aab5f23a9e7bcef0dd0219015e02324ff8b307
Ameer Hamza <ameerhamxa1811@gmail.com>
2025-07-01 02:03:37 +0500

    Update car list screen

--- lib/presentation/pages/car_list_screen.dart
+++ lib/presentation/pages/car_list_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rentapp/data/models/car.dart';
 import 'package:rentapp/presentation/bloc/car_bloc.dart';
 import 'package:rentapp/presentation/bloc/car_state.dart';
 import 'package:rentapp/presentation/widgets/car_card.dart';
 
 class CarListScreen extends StatelessWidget {
+  const CarListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +16,22 @@ class CarListScreen extends StatelessWidget {
         foregroundColor: Colors.black,
       ),
       body: BlocBuilder<CarBloc, CarState>(
-        builder: (context, state){
-          if(state is CarsLoading){
-            return Center(child: CircularProgressIndicator(),);
-          }
-          else if(state is CarsLoaded) {
+        builder: (context, state) {
+          if (state is CarsLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is CarsLoaded) {
             return ListView.builder(
               itemCount: state.cars.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 return CarCard(car: state.cars[index]);
               },
             );
-          }
-          else if(state is CarsError) {
-            return Center(child: Text('error : ${state.message}'),);
+          } else if (state is CarsError) {
+            return Center(
+              child: Text('error : ${state.message}'),
+            );
           }
           return Container();
         },
